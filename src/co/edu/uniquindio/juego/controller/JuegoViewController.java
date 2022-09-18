c9315c471ef6f2ad75f9210cb95a83d950798a65
Juan David Ortega Casas <juano_ortega@hotmail.com>
2022-09-17 19:10:58 -0500

    Prueba cronometro 7:10 pm

--- src/co/edu/uniquindio/juego/controller/JuegoViewController.java
+++ src/co/edu/uniquindio/juego/controller/JuegoViewController.java
@@ -1,5 +1,56 @@
 package co.edu.uniquindio.juego.controller;
 
-public class JuegoViewController {
+import co.edu.uniquindio.juego.Aplicacion;
+import co.edu.uniquindio.juego.model.Juego;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class JuegoViewController{
+
+   public static boolean corriendoCronometro;
+   Aplicacion aplicacion;
+   Juego juego = new Juego();
+
+    @FXML
+    private Label labelCronometro;
+
+    @FXML
+    private Button btnIniciar;
+
+    @FXML
+    void iniciar(ActionEvent event) {
+      
+      
+        (new Thread() {
+            public void run(){
+                for (int i = 0; i < 5; i++) {
+                    try {
+                    Thread.sleep(1000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                labelCronometro.setText("value " + i);
+                Platform.runLater(() -> {
+                
+                    labelCronometro.setText("value ");
+               
+                });
+                
+
+            }
+        }
+    }).start();    
+}
+
     
+    public void setAplicacion(Aplicacion aplicacion) {
+        this.aplicacion=aplicacion;
+    }
+
+
+    
+
 }
