5b1950a6813925b7eb0f1eb8cb63e2bf6052318e
Juan David Ortega Casas <juano_ortega@hotmail.com>
2022-09-17 21:02:58 -0500

    Contador organizado

--- src/co/edu/uniquindio/juego/controller/JuegoViewController.java
+++ src/co/edu/uniquindio/juego/controller/JuegoViewController.java
@@ -1,25 +1,21 @@
 package co.edu.uniquindio.juego.controller;
 
-import java.util.Timer;
-import java.util.TimerTask;
-
 import co.edu.uniquindio.juego.Aplicacion;
 import co.edu.uniquindio.juego.model.Juego;
-import javafx.application.Platform;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
-public class JuegoViewController{
+public class JuegoViewController {
 
-   public static boolean corriendoCronometro;
-   Aplicacion aplicacion;
-   Juego juego = new Juego();
-   long interval = 30;
-   boolean isCorriendo = false;
-   hilo hilo = new hilo();
- 
+    public static boolean corriendoCronometro;
+    Aplicacion aplicacion;
+    Juego juego = new Juego();
+    long interval = 30;
+    boolean isCorriendo = false;
+    hilo hilo = new hilo();
 
     @FXML
     public Label labelCronometro;
@@ -32,34 +28,35 @@ public class JuegoViewController{
 
     @FXML
     void iniciar(ActionEvent event) {
-        if(isCorriendo==false){
-            isCorriendo=true;
+
+        if (isCorriendo == false) {
+            isCorriendo = true;
             hilo = new hilo();
-            hilo.setControlador(this,isCorriendo);
+            hilo.setControlador(this, isCorriendo);
             hilo.start();
-          
-    }else{
-        System.out.println("Ya esta corriendo");
-    }
+        } else {
+            System.out.println("Ya esta corriendo");
+        }
     }
 
-    
-    
     public void setAplicacion(Aplicacion aplicacion) {
-        this.aplicacion=aplicacion;
+        this.aplicacion = aplicacion;
     }
 
     @FXML
     void parar(ActionEvent event) {
-    if(isCorriendo==true && hilo.isCorriendo==true){
-        hilo.setCorriendo(false);
-        isCorriendo=false;
-    }else{
-        System.out.println("No se puede parar el contador porque no esta corriendo");
+        if (isCorriendo == true && hilo.isCorriendo == true) {
+
+            hilo.setCorriendo(false);
+            isCorriendo = false;
+
+        } else {
+            System.out.println("No se puede parar el contador porque no esta corriendo");
+        }
     }
+
+    public void setCorriendo(boolean isCorriendo) {
+        this.isCorriendo = isCorriendo;
     }
-
-
-
 
 }
--- src/co/edu/uniquindio/juego/controller/hilo.java
+++ src/co/edu/uniquindio/juego/controller/hilo.java
@@ -2,48 +2,47 @@ package co.edu.uniquindio.juego.controller;
 
 import javafx.application.Platform;
 
-public class hilo  extends Thread{
-    
-int interval;
-JuegoViewController juegoViewController;
-boolean isCorriendo;
+public class hilo extends Thread {
 
+    int interval;
+    JuegoViewController juegoViewController;
+    boolean isCorriendo;
 
     public void run() {
-       try {
-        
-      interval=30;
+        try {
 
-        while(isCorriendo == true && interval>=0)
-        {
-            Platform.runLater(() -> juegoViewController.labelCronometro.setText(""+interval));
-            System.out.println(interval);
-            interval--;
-            Thread.sleep(200);
+            interval = 30;
+
+            while (isCorriendo == true && interval >= 0) {
+                Platform.runLater(() -> juegoViewController.labelCronometro.setText("" + interval));
+                System.out.println(interval + Thread.currentThread().getName());
+                interval--;
+                Thread.sleep(200);
+            }
+            isCorriendo = false;
+            juegoViewController.setCorriendo(false);
+        } catch (Exception e) {
+            System.out.println("Error");
         }
-        isCorriendo=false;
-    } catch (Exception e) {
-System.out.println("Error");       } 
-            
-        
-    }
 
+    }
 
     public void setControlador(JuegoViewController juegoViewController2, boolean isCorriendo) {
-        this.juegoViewController=juegoViewController2;
-        this.isCorriendo=isCorriendo;
+        this.juegoViewController = juegoViewController2;
+        this.isCorriendo = isCorriendo;
 
     }
 
-    public void setCorriendo(boolean isCorriendo){
-        this.isCorriendo=isCorriendo;
+    public void setCorriendo(boolean isCorriendo) {
+        this.isCorriendo = isCorriendo;
     }
-
 
     public boolean isCorriendo() {
         return isCorriendo;
     }
 
-    
+    public int getInterval() {
+        return interval;
+    }
 
 }
