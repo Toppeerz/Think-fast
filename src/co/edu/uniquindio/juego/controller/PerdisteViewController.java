ef374785ebf8b57e3b28c0018a0a6c96ae6dfdbe
SolenkaSaire <solenkam.sairec@uqvirtual.edu.co>
2022-10-21 01:23:01 -0500

    perdiste prueba

---
+++ src/co/edu/uniquindio/juego/controller/PerdisteViewController.java
@@ -0,0 +1,17 @@
+package co.edu.uniquindio.juego.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class PerdisteViewController {
+
+    @FXML
+    private Button btnRegresar;
+
+    @FXML
+    void regresar(ActionEvent event) {
+
+    }
+
+}
